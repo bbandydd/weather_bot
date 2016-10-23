fde2b53d86ed46015c271b709e0f1e6a301d4963
Andy Tsai <andy@Andy-Tsai.local>
2016-10-24 02:40:31 +0800

    add Facebook chat bot

--- server.js
+++ server.js
@@ -1,6 +1,18 @@
 var login = require("facebook-chat-api")
 var weather = require('./api/weather')
 
-weather(function(err, message) {
-    console.log(message)
-})
+var account = {
+    email: "FB_ACCOUNT",
+    password: "FB_PASSWORD"
+}
+
+var fb_id = '100000032727651' 
+
+// Create simple echo bot
+login(account, function callback (err, api) {
+    if(err) return console.error(err);
+
+    weather(function(err, message) {
+        api.sendMessage(message, fb_id);
+    })
+});
